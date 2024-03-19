e7d892ab1d3caa9458eabbc745aaaed776ae44cc
Shryansh <shryansh2024@gmail.com>
2024-03-19 23:21:05 +0530

    added documentation for voice Recorder molecule

--- sidebars.js
+++ sidebars.js
@@ -16,7 +16,7 @@ module.exports = {
       type: 'category',
       label: 'Molecules',
       collapsed: false,
-      items: ['molecules/overview','molecules/loginMobileAadharPage', 'molecules/otpPage']
+      items: ['molecules/overview','molecules/loginMobileAadharPage', 'molecules/otpPage', 'molecules/voiceRecorder']
     },
     {
       type: 'category',
